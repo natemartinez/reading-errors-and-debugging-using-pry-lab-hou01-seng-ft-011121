4fd072b2ccbbfc512f3d7276c802e185b986f27d
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-27 06:43:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -9,7 +9,6 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
-  puts state_log(date)
-  date = generate_star_date
+def engage(state_log)
+  puts state_log
 end
